2ce29d366b07480286162273c475c93a183399ad
Theonefoster <Theonefoster@users.noreply.github.com>
2015-12-10 21:41:57 +0000

    Update DirectImageLinkerBot.py

--- DirectImageLinkerBot.py
+++ DirectImageLinkerBot.py
@@ -12,15 +12,12 @@ userAgent = 'Removed'
 filetypes = [".png", ".gif", ".jpg", "gifv", "jpeg"]
 doneSubmissions = []
 
-url = ""
-
-bannedsubs = shelve.open('bannedSubs', 'c') # is a dictionary
-
-
 nonreplyusers = ["directimagelinkerbot", "imgurtranscriber", "automoderator", "apicontraption"] #make sure all in lower case
-
-imgurRegex = re.compile("(((http|https)\:\/\/)?(www\.|i\.|m\.)?imgur\.com\/[a-zA-Z0-9]{6,7}?(?!(\.jpg|\.gif|\.gifv|\.png|\.jpeg))(?=[^a-zA-Z0-9]|$| ))") #finds indirect imgur links only. What if there's more than one?
 bannedRegex = re.compile("you've been banned from /r/(.*)")
+bannedsubs = shelve.open('bannedSubs', 'c') # is a dictionary
+imgurRegex = re.compile("(((http|https)\:\/\/)?(www\.|i\.|m\.)?imgur\.com\/[a-zA-Z0-9]{6,7}?(?!(\.jpg|\.gif|\.gifv|\.png|\.jpeg))(?=[^a-zA-Z0-9]|$| ))") #finds indirect imgur links only. What if there's more than one?
+filetypes = [".png", ".gif", ".jpg", "gifv", "jpeg"]
+url = ""
 
 def login():
     print("Logging in..")
@@ -28,17 +25,14 @@ def login():
     r.set_oauth_app_info(appID, secret, URI)
     r.refresh_access_information(refreshToken)
     print("Logged in as " + r.user.name)
-
     return r
 
 r = login()
-
 loops = 51
 
 while True:
 
     try:
-
         loops = loops + 1
         if loops > 50:
             loops = 0
@@ -57,7 +51,6 @@ while True:
                             bannedsubs.sync()
                             msg.reply("/r/" + str(match[0]) + " has been added to my ignore list. I won't bother you again.")
                             msg.mark_as_read()
-                
 
 ############# SUBMISSIONS ############
     
@@ -66,16 +59,13 @@ while True:
             if not ((submission.author.name.lower() in nonreplyusers) or (submission.id in doneSubmissions)):
                 if (submission.subreddit.display_name not in bannedsubs['banned']):
                     doneSubmissions.append(submission.id)
-                    if (submission.domain == "imgur.com") and not ("/a/" in submission.url) and not ("gallery" in submission.url) and (not submission.url[-4:] in filetypes):
-                        
+                    if (submission.domain == "imgur.com") and not ("/a/" in submission.url) and not ("gallery" in submission.url) and (not submission.url[-4:] in filetypes):               
                         if not submission.url.startswith("http"):
                             url = "https://" + submission.url
                         else:
                            url = submission.url
-
                         submission.add_comment("[Here is a direct link to the submitted image for the benefit of mobile users](" + url + """.jpg)\n\n---\n[^Feedback](https://np.reddit.com/message/compose/?to=DirectImageLinkerBot) ^| [^Already ^a ^direct ^link?](https://np.reddit.com/r/DirectImageLinkerBot/wiki/res_links) ^| [^Why ^do ^I ^exist?](https://np.reddit.com/r/DirectImageLinkerBot/wiki/index)""")
                         print "Submission reply sent to " + submission.author.name + "!"
-
 
 ############# COMMENTS ##############
 
@@ -86,11 +76,8 @@ while True:
                 doneSubmissions.append(comment.id)
                 commentText = comment.body
                 match = list(re.findall(imgurRegex, commentText))
-
                 if (len(match) > 0):
-                    
                         athing = False #this doesn't do anything but it doesn't respond to comments wihtout it because python syntax funtimes
-
                         if len(match) == 1: #one link in comment
                             if not match[0][0].startswith("http"):
                                 url = "https://" + str(match[0][0])
@@ -101,26 +88,19 @@ while True:
                             if "gallery" not in url and "/a/" not in url:
                                 comment.reply("[Here is a direct link to that image for the benefit of mobile users](" + url + """.jpg)\n\n---\n\n^[Feedback](https://np.reddit.com/message/compose/?to=DirectImageLinkerBot) ^| [^Already ^a ^direct ^link?](https://np.reddit.com/r/DirectImageLinkerBot/wiki/res_links) ^| [^Why ^do ^I ^exist?](https://np.reddit.com/r/DirectImageLinkerBot/wiki/index)""")
                                 print "Comment reply sent to   " + comment.author.name + "!"
-
                         else:
-
                             reply = ("Here are direct links to those images for the benefit of mobile users: \n\n")
-
                             for submatch in match:
                                 if "gallery" not in submatch and "/a/" not in submatch:
-
                                    if not submatch[0].startswith("http"):
                                        url = "http://" + str(submatch[0])
                                    else:
                                        url = submatch[0]
-
                                    reply = reply + (url + ".jpg\n\n")
                             reply = reply + "---\n\n[^Feedback](https://np.reddit.com/message/compose/?to=DirectImageLinkerBot) ^| [^Already ^a ^direct ^link?](https://np.reddit.com/r/DirectImageLinkerBot/wiki/res_links) ^| [^Why ^do ^I ^exist?](https://np.reddit.com/r/DirectImageLinkerBot/wiki/index)"
 
                             comment.reply(reply)
                             print "Comment reply sent to   " + comment.author.name + "!"
-
-        
         r.handler.clear_cache()
     except praw.errors.RateLimitExceeded:
         print "Rate limit exceeded!"
@@ -129,7 +109,6 @@ while True:
         pass
     except Exception,e:
         print str(e)
-
         try:
             r = login()
         except Exception,e:
