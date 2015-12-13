e9466b55e12c1ef955d988589557d3482b5c74d4
Theonefoster <Theonefoster@users.noreply.github.com>
2015-12-13 14:36:48 +0000

    Update DirectImageLinkerBot.py

--- DirectImageLinkerBot.py
+++ DirectImageLinkerBot.py
@@ -10,7 +10,7 @@ URI = 'Removed'
 userAgent = 'Removed'
 
 doneSubmissions = []
-nonreplyusers = ["directimagelinkerbot", "imgurtranscriber", "automoderator", "apicontraption"] #make sure all in lower case
+nonreplyusers = ["directimagelinkerbot", "imgurtranscriber", "automoderator", "apicontraption", "masterjts"] #make sure all in lower case
 bannedRegex = re.compile("you've been banned from /r/(.*)")
 bannedRegex2 = re.compile("Your ban from /r/(.*) has changed")
 bannedsubs = shelve.open('bannedSubs', 'c') # is a dictionary
@@ -27,7 +27,7 @@ def login():
     return r
 
 r = login()
-loops = 51
+loops = 31
 
 while True:
 
@@ -36,13 +36,13 @@ while True:
 ############# Mail ############
 
         loops = loops + 1
-        if loops > 50:
+        if loops > 30:
             loops = 0
             msgs = list(r.get_unread(unset_has_mail=False, update_user=False))
             for msg in msgs:
                 if msg.author is not None and msg.author.name == "AutoModerator":
                     msg.mark_as_read()
-                elif ("thanks" in msg.body or "thank you" in msg.body or "thx" in msg.body) and len(msg.body) < 30:
+                elif ("thanks" in msg.body.lower() or "thank you" in msg.body.lower()) and len(msg.body) < 39:
                     msg.reply("You're welcome, human. Don't forget to direct link!")
                     msg.mark_as_read()
                 else:
@@ -58,6 +58,7 @@ while True:
                             bannedsubs.sync()
                             msg.reply("/r/" + str(match[0]) + " has been added to my ignore list. I won't bother you again.")
                             msg.mark_as_read()
+                            print "Added /r/" +  str(match[0]) + " to ignore list."
 
 ############# SUBMISSIONS ############
     
@@ -95,7 +96,7 @@ while True:
                         if "gallery" not in url and "/a/" not in url:
                             comment.reply("[Here is a direct link to your image for the benefit of mobile users](" + url + """.jpg)\n\n---\n\n^[Feedback](https://np.reddit.com/message/compose/?to=DirectImageLinkerBot&subject=Feedback&message=Don%27t%20forget%20to%20Include%20a%20link%20to%20your/my%20comment) ^| [^Already ^a ^direct ^link?](https://np.reddit.com/r/DirectImageLinkerBot/wiki/res_links) ^| [^Why ^do ^I ^exist?](https://np.reddit.com/r/DirectImageLinkerBot/wiki/index)""")
                             print "Comment reply sent to    " + comment.author.name + "!"
-                    elif len(match) < 12:
+                    elif len(match) < 10:
                         reply = ("Here are direct links to those images for the benefit of mobile users: \n\n")
                         for submatch in match:
                             if "gallery" not in submatch[0] and "/a/" not in submatch[0]:
@@ -105,7 +106,7 @@ while True:
                                     url = submatch[0]
                                 reply = reply + (url + ".jpg\n\n")
                         reply = reply + "---\n\n[^Feedback](https://np.reddit.com/message/compose/?to=DirectImageLinkerBot) ^| [^Already ^a ^direct ^link?](https://np.reddit.com/r/DirectImageLinkerBot/wiki/res_links) ^| [^Why ^do ^I ^exist?](https://np.reddit.com/r/DirectImageLinkerBot/wiki/index)"
-
+                        
                         if len(reply) > 340:
                             comment.reply(reply)
                             print "Comment reply sent to    " + comment.author.name + "!"
@@ -116,8 +117,9 @@ while True:
                             if submatch[0] not in links:
                                 links.append(submatch[0])
 
-                        if len(links) < 3:
+                        if len(links) < 3: #if number of link is >= 10 and number of unique URLs is <3 then message admins
                             r.send_message("/r/spam", "Automatic comment report", "This message was automatically submitted because the comment below contained a large number of links, but few unique images. This usually means it's a spammy message. If you'd like not to receive these messages, please reply letting me know - /u/theonefoster checks this bot's PM's daily.\n\n Link to probably-spammy comment: " + comment.permalink + "?context=3")
+                            print "Messaged admins about comment at " + comment.permalink
                    
         r.handler.clear_cache()
     except praw.errors.RateLimitExceeded,e:
